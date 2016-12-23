3ac4e19e3730a21a7dfa3c84ce359a01a2ea8a30
Robert Roach <rjayroach@gmail.com>
2016-12-23 12:32:06 +0800

    auto create a rails + ember micro service

--- lego.gemspec
+++ lego.gemspec
@@ -9,9 +9,9 @@ Gem::Specification.new do |spec|
   spec.authors       = ["Robert Roach"]
   spec.email         = ["rjayroach@gmail.com"]
 
-  spec.summary       = %q{TODO: Write a short summary, because Rubygems requires one.}
-  spec.description   = %q{TODO: Write a longer description or delete this line.}
-  spec.homepage      = "TODO: Put your gem's website or public repo URL here."
+  spec.summary       = %q{Create a micro service component with Ember and Rails}
+  spec.description   = %q{Creates a Rails 5 API server and a corresponding Ember Engine}
+  spec.homepage      = 'https://c2p4.com/lego'
 
   # Prevent pushing this gem to RubyGems.org. To allow pushes either set the 'allowed_push_host'
   # to allow pushing to a single host or delete this section to allow pushing to any host.
@@ -32,4 +32,9 @@ Gem::Specification.new do |spec|
   spec.add_development_dependency "bundler", "~> 1.13"
   spec.add_development_dependency "rake", "~> 10.0"
   spec.add_development_dependency "rspec", "~> 3.0"
+
+  spec.add_dependency "pry", "~> 0.10.4"
+  spec.add_dependency "awesome_print"
+  spec.add_dependency "rails"
+  spec.add_dependency "spring"
 end
--- lib/lego.rb
+++ lib/lego.rb
@@ -1,5 +1,132 @@
 require "lego/version"
+require 'rails'
+require 'active_record'
+require 'active_model/validations'
+# require 'spring'
 
 module Lego
-  # Your code goes here...
+  class Base
+    include ActiveModel::Model
+    include ActiveModel::Validations
+    include ActiveModel::Validations::Callbacks
+  end
+
+  class Git < Base
+    attr_accessor :service, :account
+
+    before_validation :set_defaults
+
+    validates :service, :account, presence: true
+
+    def set_defaults
+      self.service ||= 'git@github.com'
+    end
+
+    def url(name)
+      "#{service}:#{account}/#{name}.git"
+    end
+
+    def push(name)
+      system "git remote add origin #{url(name)}"
+      system "git push -u origin master"
+    end
+  end
+
+  class Project < Base
+    attr_accessor :name, :parent_dir, :dir
+
+    before_validation :set_defaults
+
+    validates :name, :parent_dir, presence: true
+
+    after_validation :make_directory
+
+    def set_defaults
+      self.parent_dir ||= Dir.home
+      self.dir = "#{parent_dir}/#{name}"
+    end
+
+    def make_directory
+      FileUtils.mkdir(dir) unless Dir.exists?(dir)
+    end
+  end
+
+  class App < Base
+    attr_accessor :project, :git, :name
+
+    before_validation :set_defaults
+
+    validates :project, :git, :name, presence: true
+    validate :associated_valid?
+
+    def associated_valid?
+      git.valid? && project.valid?
+    end
+
+    def set_defaults
+      raise NotImplementedError, 'instantiate a child class of App'
+    end
+
+    def push
+      raise 'Model invalid' if invalid?
+      Dir.chdir("#{project.dir}/#{name}") do
+        git.push(name)
+      end
+    end
+  end
+
+  class RailsApp < App
+    def set_defaults
+      self.name ||= "#{project.name}-api"
+    end
+
+    def create
+      return if invalid? or Dir.exists?("#{project.dir}/#{name}")
+      Dir.chdir(project.dir) do
+        system "rails new #{name} --api -m ~/rails-templates/templates/rails5/api.rb"
+        system 'git add .'
+        system "git commit -m 'Initial Commit from Rails'"
+      end
+    end
+  end
+
+  class EmberApp < App
+    def set_defaults
+      self.name ||= "#{project.name}-app"
+    end
+
+    def create
+      return if invalid? or Dir.exists?("#{project.dir}/#{name}")
+      Dir.chdir(project.dir) do
+        system "ember new #{name}"
+        # TODO: Add the default addons here
+        # system "ember install ember-simple-auth"
+      end
+    end
+  end
+
+  class MicroService < Base
+    attr_accessor :account, :name, :rails, :ember
+
+    after_validation :initialize_apps
+
+    def initialize_apps
+      git = Git.new(account: account)
+      project = Project.new(name: name)
+      self.rails = RailsApp.new(git: git, project: project)
+      self.ember = EmberApp.new(git: git, project: project)
+    end
+
+    def create
+      return if invalid?
+      rails.create
+      ember.create
+    end
+
+    def push
+      return if invalid?
+      rails.push
+      ember.push
+    end
+  end
 end
